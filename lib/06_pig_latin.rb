f3b8102bdfc713d49a2ed20a06c958cfe9aaed2e
Chreees79 <christelle.gevaert32@gmail.com>
2022-07-11 23:07:03 +0200

    refacto#2 pig_latin

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -4,28 +4,28 @@ translate_word = []
   word_array.split(" ").each do |word| 
     # all the code execute : "translates many words" and "translates two words"
 
-    if word.start_with?('a', 'e', 'i', 'o', 'u')  
-      translate_word << word + 'ay'
+    if word.start_with?(/[aeiou]/)  
+      translate_word.push( word + 'ay')
     # translates a word beginning with a vowel
     
-    elsif word.start_with?('q', 'u')
-      translate_word << "#{word.byteslice(2..100)}quay"
+    elsif word.start_with?(/[qu]/)
+      translate_word.push("#{word.byteslice(2,(word.size)) + word[0..1]}ay")
     # "counts 'qu' as a single phoneme"
     
-    elsif word.start_with?('s') && word[1..100].start_with?('q', 'u')
-      translate_word << "#{word.byteslice(3,3) + word [0..2]}ay"
+    elsif word.start_with?(/[s]/) && word[1..2].start_with?(/[qu]/)
+      translate_word.push("#{word.byteslice(3,(word.size)) + word [0..2]}ay")
     #  "counts 'qu' as a consonant even when it's preceded by a consonant"  
     
-    elsif word[1..(word.size)].start_with?('a', 'e', 'i', 'o', 'u')
-      translate_word << "#{word.byteslice(1,100) + word[0]}ay"
+    elsif word[1..(word.size)].start_with?(/[aeiou]/)
+      translate_word.push("#{word.byteslice(1,(word.size)) + word[0]}ay")
     # "translates a word beginning with one consonant"
 
-    elsif word[2..(word.size)].start_with?('a', 'e', 'i', 'o', 'u')
-      translate_word << "#{word.byteslice(2,10) + word[0..1]}ay"
+    elsif word[2..(word.size)].start_with?(/[aeiou]/)
+      translate_word.push("#{word.byteslice(2,(word.size)) + word[0..1]}ay")
     # "translates a word beginning with two consonants"
  
-    else word[3..(word.size)].start_with?('a', 'e', 'i', 'o', 'u') 
-      translate_word <<"#{word.byteslice(3, 3) + word[0..2]}ay"
+    else word[3..(word.size)].start_with?(/[aeiou]/) 
+      translate_word.push("#{word.byteslice(3, (word.size)) + word[0..2]}ay")
     #"translates a word beginning with three consonants"
 
     end
